ae28a58b488d47854d74c85781678f1fcf234853
Rui Pereira <me@ruipereira.dev>
2021-04-29 22:11:33 +0100

    Get block by ID

--- pkg/protocol/blocks/blocks.go
+++ pkg/protocol/blocks/blocks.go
@@ -9,13 +9,28 @@ import (
 var (
 	//go:embed blocks.json
 	blocksFile []byte
+
 	blocks     map[string]map[int]int
+	blocksByID map[int]map[int]string
 )
 
 func init() {
 	err := json.Unmarshal(blocksFile, &blocks)
 	if err != nil {
 		panic(err)
+	}
+
+	blocksByID = make(map[int]map[int]string)
+	for block, data := range blocks {
+		for proto, id := range data {
+			if a, ok := blocksByID[id]; ok {
+				a[proto] = block
+			} else {
+				blocksByID[id] = map[int]string{
+					proto: block,
+				}
+			}
+		}
 	}
 }
 
@@ -33,4 +48,20 @@ func GetBlockID(block string, protocol protocol.Protocol) int {
 		}
 	}
 	return lastID
+}
+
+func GetBlock(id int, protocol protocol.Protocol) string {
+	lastProto := 0
+	lastBlock := "minecraft:air"
+	for proto, block := range blocksByID[id] {
+		if proto == int(protocol) {
+			return block
+		}
+
+		if int(protocol) > proto && proto > lastProto {
+			lastProto = proto
+			lastBlock = block
+		}
+	}
+	return lastBlock
 }
